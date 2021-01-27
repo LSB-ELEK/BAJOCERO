82ab41299b56abe67064a79b266063055bfe061c
JBarrioclase <bafeju@lsbikasleak.eus>
2021-01-27 16:24:24 +0100

    srdgthfyjgkh

--- Codigo_General/Codigo_General_V4/Codigo_General_V4.ino
+++ Codigo_General/Codigo_General_V4/Codigo_General_V4.ino
@@ -59,7 +59,7 @@ void setup() {
 
 void loop() {
 
-  if(millis()-ant_millis>5000){
+  if(millis()-ant_millis>19000){
     ant_millis = millis();
   datos = RTC() + GPS() + BAROMETRO() + CCS811() + dht22() + TSL() + LuzUV() ;
   SDdatalogger(datos);
@@ -70,7 +70,7 @@ void loop() {
   contador_datos++;
 
   }
-  if(contador_datos > 3){
+  if(contador_datos > 100){
       contador_archivos++;
       ARCHIVO = "dato_" + String(contador_archivos) + ".csv";
       contador_datos = 1;
@@ -159,7 +159,7 @@ String BAROMETRO(){
     String altitud = "";
     temperatura = bmp.readTemperature();
     presion = bmp.readPressure()/100;
-    altitud = bmp.readAltitude(1013.25);
+    altitud = bmp.readAltitude(1005.1);
     String b = temperatura + ", " + presion + ", " + altitud + ", ";
     return b;
   }
