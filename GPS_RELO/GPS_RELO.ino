86ec114b65dd55b735a0ed62978b3012aca7c911
JBarrioclase <bafeju@lsbikasleak.eus>
2021-01-28 08:11:06 +0100

    Update GPS_RELO.ino

--- GPS_RELO/GPS_RELO.ino
+++ GPS_RELO/GPS_RELO.ino
@@ -1,14 +1,37 @@
-#include <TinyGPS.h>
 
+#include <RTClib.h>
+#include <TinyGPS.h>
+#include <LiquidCrystal_I2C.h>
+
+TinyGPS gps;
+LiquidCrystal_I2C LCD(0x27, 16, 2);
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  LCD.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool newData = 0;
+  String Datos;
+
+  {
+      while (Serial1.available())
+      {
+         char c = Serial1.read();  //Guarda dentro de c los valores que de el GPS
+         if (gps.encode(c))  //Desodifica la nueva secuencia recibida
+            newData = true;
+      }
+   }
+    if (newData){
+        Datos = gps.time.hour();
+        Serial.println(Datos);
+        
+         }
+   
 
 }
